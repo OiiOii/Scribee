6e6b46c760980c22836315ee2fb00cf2bd9f2b32
razahuss121 <razahuss121@gmail.com>
2024-04-20 13:53:52 -0400

    extra testing file

---
+++ tests/extraTest.js
@@ -0,0 +1,309 @@
+import { note } from "../scripts/note";
+
+let testTitle1 = "test title 1";
+let testDesc1 = "test desc 1";
+let testDate1 = '1999-08-08';
+let testPinned1 = false;
+
+let testNote1 = new note(testTitle1, testDesc1, testDate1);
+
+if(testNote1.getTitle === testTitle1 && testNote1.getText === testDesc1
+   && testNote1.getDate === testDate1 && testNote1.getPinned === testPinned1){
+    console.log("test 1 passed")
+   }
+   else{
+        console.log("test 1 failed");
+   }
+
+   let testTitle2 = "test title 2";
+   let testDesc2 = "test desc 2";
+   let testDate2 = '1999-08-08';
+   let testPinned2 = false;
+   
+   let testNote2 = new note(testTitle2, testDesc2, testDate2);
+   
+   if(testNote2.getTitle === testTitle2 && testNote2.getText === testDesc2
+      && testNote2.getDate === testDate2 && testNote2.getPinned === testPinned2){
+       console.log("test 2 passed")
+      }
+      else{
+           console.log("test 2 failed");
+      }
+   
+let testTitle3 = "test title 3";
+let testDesc3 = "test desc 3";
+let testDate3 = '1999-03-04';
+let testPinned3 = false;
+
+let testNote3 = new note(testTitle3, testDesc3, testDate3);
+
+if(testNote3.getTitle === testTitle3 && testNote3.getText === testDesc3
+   && testNote3.getDate === testDate3 && testNote3.getPinned === testPinned3){
+    console.log("test 3 passed")
+   }
+   else{
+        console.log("test 3 failed");
+   }
+
+let testTitle4 = "test title 4";
+let testDesc4 = "test desc 4";
+let testDate4 = '1999-04-04';
+let testPinned4 = false;
+
+let testNote4 = new note(testTitle4, testDesc4, testDate4);
+
+if(testNote4.getTitle === testTitle4 && testNote4.getText === testDesc4
+   && testNote4.getDate === testDate4 && testNote4.getPinned === testPinned4){
+    console.log("test 4 passed")
+   }
+   else{
+        console.log("test 4 failed");
+   }
+
+let testTitle5 = "test title 5";
+let testDesc5 = "test desc 5";
+let testDate5 = '1999-03-03';
+let testPinned5 = false;
+
+let testNote5 = new note(testTitle5, testDesc5, testDate5);
+
+if(testNote5.getTitle === testTitle5 && testNote5.getText === testDesc5
+   && testNote5.getDate === testDate5 && testNote5.getPinned === testPinned5){
+    console.log("test 5 passed")
+   }
+   else{
+        console.log("test 5 failed");
+   }
+
+let testTitle6 = "test title 6";
+let testDesc6 = "test desc 6";
+let testDate6 = '1999-03-03';
+let testPinned6 = false;
+
+let testNote6 = new note(testTitle6, testDesc6, testDate6);
+
+if(testNote6.getTitle === testTitle6 && testNote6.getText === testDesc6
+   && testNote6.getDate === testDate6 && testNote6.getPinned === testPinned6){
+    console.log("test 6 passed")
+   }
+   else{
+        console.log("test 6 failed");
+   }
+
+   let testTitle7 = "test title 7";
+   let testDesc7 = "test desc 7";
+   let testDate7 = '1998-00-03';
+   let testPinned7 = false;
+   
+   let testNote7 = new note(testTitle7, testDesc7, testDate7);
+   
+   if(testNote7.getTitle === testTitle7 && testNote7.getText === testDesc7
+      && testNote7.getDate === testDate7 && testNote7.getPinned === testPinned7){
+       console.log("test 7 passed")
+      }
+      else{
+           console.log("test 7 failed");
+      }
+
+let testTitle8 = "test title 8";
+let testDesc8 = "test desc 8";
+let testDate8 = '1918-08-08';
+let testPinned8 = false;
+
+let testNote8 = new note(testTitle8, testDesc8, testDate8);
+
+if(testNote8.getTitle === testTitle8 && testNote8.getText === testDesc8
+   && testNote8.getDate === testDate8 && testNote8.getPinned === testPinned8){
+    console.log("test 8 passed")
+   }
+   else{
+        console.log("test 8 failed");
+   }
+
+let testTitle9 = "test title 9";
+let testDesc9 = "test desc 9";
+let testDate9 = '1999-08-08';
+let testPinned9 = false;
+
+let testNote9 = new note(testTitle9, testDesc9, testDate9);
+
+if(testNote9.getTitle === testTitle9 && testNote9.getText === testDesc9
+   && testNote9.getDate === testDate9 && testNote9.getPinned === testPinned9){
+    console.log("test 9 passed")
+   }
+   else{
+        console.log("test 9 failed");
+   }
+
+   let testTitle10 = "test title 10";
+   let testDesc10 = "test desc 10";
+   let testDate10 = '1999-08-08';
+   let testPinned10 = false;
+   
+   let testNote10 = new note(testTitle10, testDesc10, testDate10);
+   
+   if(testNote10.getTitle === testTitle10 && testNote10.getText === testDesc10
+      && testNote10.getDate === testDate10 && testNote10.getPinned === testPinned10){
+       console.log("test 10 passed")
+      }
+      else{
+           console.log("test 10 failed");
+      }
+
+      let testTitle11 = "test title 11";
+   let testDesc11 = "test desc 11";
+   let testDate11 = '1999-08-08';
+   let testPinned11 = false;
+   
+   let testNote11 = new note(testTitle11, testDesc11, testDate11);
+   
+   if(testNote11.getTitle === testTitle11 && testNote11.getText === testDesc11
+      && testNote11.getDate === testDate11 && testNote11.getPinned === testPinned11){
+       console.log("test 11 passed")
+      }
+      else{
+           console.log("test 11 failed");
+      }
+
+
+      let testTitle113 = "test title 113";
+   let testDesc113 = "test desc 113";
+   let testDate113 = '1999-08-08';
+   let testPinned113 = false;
+   
+   let testNote113 = new note(testTitle113, testDesc113, testDate113);
+   
+   if(testNote113.getTitle === testTitle113 && testNote113.getText === testDesc113
+      && testNote113.getDate === testDate113 && testNote113.getPinned === testPinned113){
+       console.log("test 113 passed")
+      }
+      else{
+           console.log("test 113 failed");
+      }
+
+
+      let testTitle13 = "test title 13";
+   let testDesc13 = "test desc 13";
+   let testDate13 = '1999-08-08';
+   let testPinned13 = false;
+   
+   let testNote13 = new note(testTitle13, testDesc13, testDate13);
+   
+   if(testNote13.getTitle === testTitle13 && testNote13.getText === testDesc13
+      && testNote13.getDate === testDate13 && testNote13.getPinned === testPinned13){
+       console.log("test 13 passed")
+      }
+      else{
+           console.log("test 13 failed");
+      }
+
+
+      let testTitle14 = "test title 14";
+   let testDesc14 = "test desc 14";
+   let testDate14 = '1999-08-08';
+   let testPinned14 = false;
+   
+   let testNote14 = new note(testTitle14, testDesc14, testDate14);
+   
+   if(testNote14.getTitle === testTitle14 && testNote14.getText === testDesc14
+      && testNote14.getDate === testDate14 && testNote14.getPinned === testPinned14){
+       console.log("test 14 passed")
+      }
+      else{
+           console.log("test 14 failed");
+      }
+
+
+      let testTitle15 = "test title 15";
+   let testDesc15 = "test desc 15";
+   let testDate15 = '1999-08-08';
+   let testPinned15 = false;
+   
+   let testNote15 = new note(testTitle15, testDesc15, testDate15);
+   
+   if(testNote15.getTitle === testTitle15 && testNote15.getText === testDesc15
+      && testNote15.getDate === testDate15 && testNote15.getPinned === testPinned15){
+       console.log("test 15 passed")
+      }
+      else{
+           console.log("test 15 failed");
+      }
+      
+
+      let testTitle16 = "test title 16";
+   let testDesc16 = "test desc 16";
+   let testDate16 = '1999-08-08';
+   let testPinned16 = false;
+   
+   let testNote16 = new note(testTitle16, testDesc16, testDate16);
+   
+   if(testNote16.getTitle === testTitle16 && testNote16.getText === testDesc16
+      && testNote16.getDate === testDate16 && testNote16.getPinned === testPinned16){
+       console.log("test 16 passed")
+      }
+      else{
+           console.log("test 16 failed");
+      }
+
+
+      let testTitle17 = "test title 17";
+   let testDesc17 = "test desc 17";
+   let testDate17 = '1999-08-08';
+   let testPinned17 = false;
+   
+   let testNote17 = new note(testTitle17, testDesc17, testDate17);
+   
+   if(testNote17.getTitle === testTitle17 && testNote17.getText === testDesc17
+      && testNote17.getDate === testDate17 && testNote17.getPinned === testPinned17){
+       console.log("test 17 passed")
+      }
+      else{
+           console.log("test 17 failed");
+      }
+   
+   
+   
+      let testTitle18 = "test title 18";
+      let testDesc18 = "test desc 18";
+      let testDate18 = '1999-08-08';
+      let testPinned18 = false;
+      
+      let testNote18 = new note(testTitle18, testDesc18, testDate18);
+      
+      if(testNote18.getTitle === testTitle18 && testNote18.getText === testDesc18
+         && testNote18.getDate === testDate18 && testNote18.getPinned === testPinned18){
+          console.log("test 18 passed")
+         }
+         else{
+              console.log("test 18 failed");
+         }
+
+         let testTitle19 = "test title 19";
+   let testDesc19 = "test desc 19";
+   let testDate19 = '1999-08-08';
+   let testPinned19 = false;
+   
+   let testNote19 = new note(testTitle19, testDesc19, testDate19);
+   
+   if(testNote19.getTitle === testTitle19 && testNote19.getText === testDesc19
+      && testNote19.getDate === testDate19 && testNote19.getPinned === testPinned19){
+       console.log("test 19 passed")
+      }
+      else{
+           console.log("test 19 failed");
+      }
+
+      let testTitle20 = "test title 20";
+   let testDesc20 = "test desc 20";
+   let testDate20 = '1999-08-08';
+   let testPinned20 = false;
+   
+   let testNote20 = new note(testTitle20, testDesc20, testDate20);
+   
+   if(testNote20.getTitle === testTitle20 && testNote20.getText === testDesc20
+      && testNote20.getDate === testDate20 && testNote20.getPinned === testPinned20){
+       console.log("test 20 passed")
+      }
+      else{
+           console.log("test 20 failed");
+      }
